6c65d20c6d48610db2ef6f3eaa611cd7ee6d9ac6
LuskasHusty <63302125+LuskasHusty@users.noreply.github.com>
2022-08-04 15:40:08 -0300

    Updated Size calculation

--- src/domain/snode/dentry/DEntry.java
+++ src/domain/snode/dentry/DEntry.java
@@ -6,17 +6,15 @@ import src.domain.snode.SNode;
 
 public class DEntry {
     private final SNode sNode; //isso pode ser tanto um snode de diretorio como de arquivo
-    private final int entryLength;
     private final FileType fileType;
     private final String fileName;
 
     private final int size;
 
-    public DEntry( SNode snode, int entryLength, FileType fileType, String fileName ) 
+    public DEntry( SNode snode, FileType fileType, String fileName ) 
     throws InvalidEntryException
     {
         this.sNode = snode;
-        this.entryLength = entryLength;
         this.fileType = fileType;
     
 
@@ -27,10 +25,12 @@ public class DEntry {
                 
 
         this.fileName = fileName; 
-        //TODO O size tem que arredondar para proxima potencia de 2
 
     
-        this.size = 6 + fileName.length(); //size será o tamanho da estrutura DEntry. O tamanho máximo do DEntry deve ser menor que 128 bytes 
+        this.size = (6 + fileName.length()) + (6 + fileName.length())%16;
+
+        
+        //size será o tamanho da estrutura DEntry. O tamanho máximo do DEntry deve ser menor que 128 bytes 
         
 
 
