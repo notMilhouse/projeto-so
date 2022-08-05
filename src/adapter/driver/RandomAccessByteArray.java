6e08eeac4348a8a1eeb5f4a5362329f4caf06374
LuskasHusty <63302125+LuskasHusty@users.noreply.github.com>
2022-08-04 21:30:15 -0300

    Clean up!

--- src/adapter/driver/RandomAccessByteArray.java
+++ src/adapter/driver/RandomAccessByteArray.java
@@ -77,39 +77,4 @@ public class RandomAccessByteArray
         return wrapper.getLong();
     }
 
-
-
-    
-    /*
-    public byte[] ReadBytes(int numberOfBytes)
-    throws IOException
-    {
-        byte[] byteArray = new byte[numberOfBytes];
-        diskAccess.read(byteArray, 0, numberOfBytes);
-        return byteArray;
-    }
-
-    public long ByteArrayToLong(byte[] byteArray)
-    {
-        ByteBuffer wrapper = ByteBuffer.wrap(byteArray);
-        return wrapper.getLong();
-    }
-    public int ByteArrayToInt(byte[] byteArray)
-    {
-        ByteBuffer wrapper = ByteBuffer.wrap(byteArray);
-        return wrapper.getInt();
-    }
-    public int ByteArrayToUnsignedShort(byte[] byteArray)
-    {
-        ByteBuffer wrapper = ByteBuffer.wrap(byteArray);
-        short ShortValue = wrapper.getShort();
-        return ShortValue >= 0? ShortValue : 0x10000 + ShortValue;
-    }
-    public short ByteArrayToShort(byte[] byteArray)
-    {
-        ByteBuffer wrapper = ByteBuffer.wrap(byteArray);
-        return wrapper.getShort();
-    }
-    */
-
 }
